89d6bc4483893f77a36f7e7b35a1c3cb6be72d87
switched-on-steve <stephenjmoylan@gmail.com>
2023-01-13 19:57:49 +1000

    again updated

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ const port = process.env.PORT || 3001;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => res.type('html').send(html));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-const html = `
-<!DOCTYPE html>
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/html')
+  let response = `
+  <!DOCTYPE html>
   <html>
   <head>
     <style>
@@ -30,13 +27,19 @@ const html = `
   </head>
   <body>
     <div>
-      <h3>Request Information</h3>
+      <h1>Your Request Information</h1>
       <ul>
-        <li>Path: ${req.originalUrl}</li>
+        <li>Path: ${req.path}</li>
         <li>Query String: ${JSON.stringify(req.query)}</li>
         <li>Headers: <pre>${JSON.stringify(req.headers, null, 2)}</pre></li>
       </ul>
     </div>
   </body>
   </html>
-`
+  `
+  res.send(response)
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+})
